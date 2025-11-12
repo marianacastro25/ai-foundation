69d628fd4e6d847f7e62928dd2f106955a526f88
marianacastro25 <marianamoreiracastro2003@gmail.com>
2025-11-12 18:09:00 +0100

    11_11

--- winequality.py
+++ winequality.py
@@ -1,9 +1,27 @@
-import csv
+'import data base'
 
-with open("winequality-red.csv", "r") as f:
-    reader = csv.reader(f, delimiter="\t")
-    for i, line in enumerate(reader):
-        print(i, line)
-       
-        
-       
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\maria\Documents\ai-foundation\winequality-red.csv", sep=';')
+print(df.head())
+
+'''
+df.info()
+print(df['quality'].value_counts().sort_index())
+
+print(df.isnull().sum())
+'''
+
+print(df.nunique())
+
+print(df.duplicated().sum())
+
+df.drop_duplicates(inplace = True)
+df = df.reset_index(drop=True)
+
+num_cols = [col for col in df.columns if (df[col].dtype in ["int64","float64"]) & (df[col].nunique() > 50)]
+print(num_cols)
+target = [col for col in df.columns if df[col].nunique()<10]
+print(target)
+
+
